93a6903f948407e74143abb4a33a7113b562456c
Lutfulla22 <nlutfulla2112@mail.ru>
2021-11-02 23:34:53 +0500

    console

---
+++ Startup.cs
@@ -0,0 +1,45 @@
+using efconsole.Data;
+using efconsole.services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace efconsole
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; private set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ConsoleDbContext>(
+                options =>
+                {
+                    if (Configuration.GetValue<bool>("UseInMemoryDatabase"))
+                    {
+                        options.UseSqlite(Configuration.GetConnectionString("SqliteConnection"));
+                    }
+                    else
+                    {
+                        options.UseSqlServer(Configuration.GetConnectionString("SqlServerConnection"));
+                    }
+                }, ServiceLifetime.Singleton);
+
+            services.AddHostedService<ConsoleService>();
+            services.AddTransient<IStorageTeacherService, DbStorageService>();
+            services.AddTransient<InternalStorageService>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+        }
+    }
+}
